84f010cc6bbc02a753e8cd94231089c449827f65
Tampoka <goal.thanks@gmail.com>
2021-11-03 21:38:27 +0900

    finished Skills block

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import s from './Main.module.css'
+import styleContainer from './../common/styles/Container.module.css'
 
 const Main = () => {
     return (
         <div className={s.mainBlock}>
-            <div className={s.container}>
+            <div className={styleContainer.container}>
                 <div className={s.intro}>
                     <span>Hi there</span>
                     <h1>I am Robot-coder</h1>
---
+++ src/Skills/Skills.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import s from './Skills.module.css'
+import styleContainer from './../common/styles/Container.module.css'
+import Skill from "./Skill/Skill";
+
+
+const Skills = () => {
+    return (
+        <div className={s.skillsBlock}>
+            <div className={`${styleContainer.container} ${s.skillsContainer}`}>
+                <h2 className={s.title}>My skills</h2>
+                <div className={s.skills}>
+                    <Skill title="JS" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, nulla."/>
+                    <Skill title="React" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, nulla."/>
+                    <Skill title="Redux" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, nulla."/>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Skills;
