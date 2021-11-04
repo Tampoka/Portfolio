0d3ae15f80e8a6f8d54f6790f77a5bd1b1f4dfc4
Tampoka <goal.thanks@gmail.com>
2021-11-04 09:45:42 +0900

    finished Contacts

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main/Main";
 import Skills from "./Skills/Skills";
 import Projects from "./Projects/Projects";
 import Additional from "./Additional/Additional";
+import Contacts from "./Contacts/Contacts";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <Skills/>
             <Projects/>
             <Additional/>
+            <Contacts/>
         </div>
     );
 }
---
+++ src/Contacts/Contacts.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import s from './Contacts.module.css'
+import styleContainer from './../common/styles/Container.module.css'
+
+
+const Contacts = () => {
+    return (
+        <div className={s.contacts}>
+    <div className={`${styleContainer.container} ${s.container}`}>
+        <h2>Contacts</h2>
+        <form className={s.form}>
+            <input className={s.input} type="text" placeholder="Your Name"/>
+            <input className={s.input} type="email" placeholder="Your email"/>
+            <textarea className={s.textarea} name="" id="" cols="30" rows="10"
+                      placeholder="Questions, concerns...">
+
+            </textarea>
+        </form>
+        <button className={s.btn}>Send</button>
+    </div>
+        </div>
+    );
+};
+
+export default Contacts;
