2f3ce073febcced9914390adece7c019b8b2e565
Tampoka <goal.thanks@gmail.com>
2021-11-07 19:25:55 +0900

    Made image to be background one

--- src/Projects/Project/Project.jsx
+++ src/Projects/Project/Project.jsx
@@ -5,9 +5,6 @@ const Project = (props) => {
     return (
         <div className={s.project}>
             <div className={s.imgContainer}>
-                <img className={s.image}
-                     src="https://image.shutterstock.com/image-photo/istanbul-turkey-july-2021-illustrative-260nw-2019755717.jpg"
-                     alt="Project screenshot"/>
                 <a href='#'>Show more</a>
             </div>
             <h3>{props.projectTitle}</h3>
