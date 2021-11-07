1b422fc41f8fe5ade552ab7c5261e40eddfd4ec9
Tampoka <goal.thanks@gmail.com>
2021-11-07 17:34:55 +0900

    Add link to project

--- src/Projects/Project/Project.jsx
+++ src/Projects/Project/Project.jsx
@@ -4,9 +4,12 @@ import s from './Project.module.css'
 const Project = (props) => {
     return (
         <div className={s.project}>
-            <img className={s.image}
-                src="https://image.shutterstock.com/image-photo/istanbul-turkey-july-2021-illustrative-260nw-2019755717.jpg"
-                alt="Project screenshot"/>
+            <div className={s.imgContainer}>
+                <img className={s.image}
+                     src="https://image.shutterstock.com/image-photo/istanbul-turkey-july-2021-illustrative-260nw-2019755717.jpg"
+                     alt="Project screenshot"/>
+                <a href='#'>Show more</a>
+            </div>
             <h3>{props.projectTitle}</h3>
             <p>{props.projectDescription}</p>
         </div>
