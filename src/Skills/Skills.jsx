cf16051cda4c4b1d3814986693bd1fc0e5a13360
Tampoka <goal.thanks@gmail.com>
2021-11-17 22:46:55 +0900

    Add BlockTitle component

--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import s from './Skills.module.css'
 import styleContainer from './../common/styles/Container.module.css'
 import Skill from "./Skill/Skill";
+import BlockTitle from "../common/components/blockTitle/BlockTitle";
 
 
 const Skills = () => {
     return (
         <div className={s.skillsBlock}>
             <div className={`${styleContainer.container} ${s.skillsContainer}`}>
-                <h2 className={s.title}>My skills</h2>
+                <BlockTitle title="My skills"/>
                 <div className={s.skills}>
                     <Skill title="JS" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, nulla."/>
                     <Skill title="React" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, nulla."/>
---
+++ src/common/components/blockTitle/BlockTitle.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from "./BlockTitle.module.css";
+
+const BlockTitle = (props) => {
+    return (
+        <div className={s.title}><h2>{props.title}</h2></div>
+    );
+};
+
+export default BlockTitle;
