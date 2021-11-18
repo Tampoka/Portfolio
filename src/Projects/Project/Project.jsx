83ae8a1ac3fd1a3c5e2dbdb3cbe4adf5a5bbce3d
Tampoka <goal.thanks@gmail.com>
2021-11-18 09:27:01 +0900

    Style projects images

--- src/Projects/Project/Project.jsx
+++ src/Projects/Project/Project.jsx
@@ -5,7 +5,7 @@ const Project = (props) => {
     return (
         <div className={s.project}>
             <div className={s.imgContainer} style={props.style}>
-                <a href='#'>Show more</a>
+                <a href='#' className={s.btn}>Show more</a>
             </div>
             <div className={s.projectInfo}><h3 className={s.projectTitle}>{props.projectTitle}</h3>
                 <p className={s.description}>{props.projectDescription}</p></div>
